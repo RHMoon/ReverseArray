92f80242606653d81fa846306c68deedfd8bd2e4
Naufal Muhammad Farras <nmfarras@gmail.com>
2021-10-09 00:42:12 +0700

    Add loop for checking biggest and smallest value from array

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Integer [] numArray = {10,2,8,3,1,5,9,14,6};
+        int biggest = numArray[0];
+        int smallest = numArray[0];
+
+        for (int i = 0; i < numArray.length; i++) {
+            if (numArray[i] > biggest) {
+                biggest = numArray[i];
+            }
+            if (numArray[i] < smallest) {
+                smallest = numArray[i];
+            }
+        }
+
+        System.out.println("Smallest number: " + smallest);
+        System.out.println("Biggest number: " + biggest);
     }
 }
